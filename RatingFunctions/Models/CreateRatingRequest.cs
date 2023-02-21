e8cd2d2d65747e87bfdff4ed4606872321871821
Baiju Nagori <bnagori@fi.com>
2023-02-21 14:43:55 -0600

    Initial commit

---
+++ RatingFunctions/Models/CreateRatingRequest.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace RatingFunctions.Models
+{
+    public class CreateRatingRequest
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("userId")]
+        public string UserId { get; set; }
+
+        [JsonProperty("productId")]
+        public string ProductId { get; set; }
+
+        [JsonProperty("locationName")]
+        public string LocationName { get; set; }
+
+        [JsonProperty("rating")]
+        public int Rating { get; set; }
+
+        [JsonProperty("userNotes")]
+        public string UserNotes { get; set; }
+    }
+
+    
+}
